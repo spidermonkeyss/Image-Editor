8c2254f819c63043cb7d1169710d8b0468ca14a4
Andrew Hill <andrewhill@columbus.rr.com>
2023-01-31 11:04:58 -0500

    Inital commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ImageEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pickColorBtn = new System.Windows.Forms.Button();
+            this.colorDisplayTextBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(158, 34);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(169, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(541, 278);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // pickColorBtn
+            // 
+            this.pickColorBtn.Location = new System.Drawing.Point(33, 163);
+            this.pickColorBtn.Name = "pickColorBtn";
+            this.pickColorBtn.Size = new System.Drawing.Size(93, 49);
+            this.pickColorBtn.TabIndex = 2;
+            this.pickColorBtn.Text = "Pick color";
+            this.pickColorBtn.UseVisualStyleBackColor = true;
+            this.pickColorBtn.Click += new System.EventHandler(this.pickColorBtn_Click);
+            // 
+            // colorDisplayTextBox
+            // 
+            this.colorDisplayTextBox.BackColor = System.Drawing.Color.Black;
+            this.colorDisplayTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.colorDisplayTextBox.Cursor = System.Windows.Forms.Cursors.Default;
+            this.colorDisplayTextBox.Enabled = false;
+            this.colorDisplayTextBox.ForeColor = System.Drawing.SystemColors.Window;
+            this.colorDisplayTextBox.Location = new System.Drawing.Point(66, 218);
+            this.colorDisplayTextBox.Name = "colorDisplayTextBox";
+            this.colorDisplayTextBox.Size = new System.Drawing.Size(26, 26);
+            this.colorDisplayTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.colorDisplayTextBox);
+            this.Controls.Add(this.pickColorBtn);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button pickColorBtn;
+        private System.Windows.Forms.TextBox colorDisplayTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ImageEditor
+{
+    public partial class Form1 : Form
+    {
+        Bitmap panelImageBitmap;
+        bool isMouseDraw = false;
+        int prevMouseX, prevMouseY;
+        Color drawColor = Color.Black;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            panelImageBitmap = new Bitmap(panel1.Width, panel1.Height);
+            for (int y = 0; y < panel1.Height; y++)
+            {
+                for (int x = 0; x < panel1.Width; x++)
+                {
+                    panelImageBitmap.SetPixel(x, y, Color.White);
+                }
+            }
+            UpdatePanelImage();
+
+            //This prevents flickering in the panel with the image
+            typeof(Panel).InvokeMember("DoubleBuffered",
+            BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+            null, panel1, new object[] { true });
+        }
+
+        private void UpdatePanelImage()
+        {
+            panel1.BackgroundImage = panelImageBitmap;
+
+            panel1.Invalidate();
+            panel1.Update();
+        }
+
+        private void DrawPixel(int x, int y)
+        {
+            if (x < 0 || x >= panelImageBitmap.Width)
+            {
+                Console.WriteLine("x out of panel");
+                isMouseDraw = false;
+                return;
+            }
+            if (y < 0 || y >= panelImageBitmap.Height)
+            {
+                Console.WriteLine("y out of panel");
+                isMouseDraw = false;
+                return;
+            }
+
+            panelImageBitmap.SetPixel(x, y, drawColor);
+            prevMouseX = x;
+            prevMouseY = y;
+
+            UpdatePanelImage();
+        }
+
+        private void DrawLineBetweenPoints(int startX, int startY, int endX, int endY)
+        {
+            float m, b;
+            int botY, topY;
+
+            //Mouse not moving left or right
+            if (startX == endX)
+            {
+                Console.Write("e");
+                //Mouse moving up
+                if (startY < endY)
+                {
+                    for (int y = startY; y <= endY; y++)
+                    {
+                        DrawPixel(startX, y);
+                    }
+                }
+                //Mouse moving down
+                else
+                {
+                    for (int y = startY; y >= endY; y--)
+                    {
+                        DrawPixel(startX, y);
+                    }
+                }
+            }
+            //Mouse moving right
+            else if (startX < endX)
+            {
+                m = (float)(endY - startY) / (float)(endX - startX);
+                b = startY - (m * startX);
+                //Mouse moving down
+                if (startY < endY)
+                {
+                    for (int x = startX; x <= endX; x++)
+                    {
+                        botY = (int)Math.Floor((m * (x - 1)) + b);
+                        topY = (int)Math.Floor((m * x) + b);
+                        for (int y = botY; y <= topY; y++)
+                        {
+                            DrawPixel(x, y);
+                        }
+                    }
+                }
+                //Mouse moving up
+                else
+                {
+                    for (int x = startX; x <= endX; x++)
+                    {
+                        botY = (int)Math.Floor((m * x) + b);
+                        topY = (int)Math.Floor((m * (x - 1)) + b);
+                        for (int y = topY; y >= botY; y--)
+                        {
+                            DrawPixel(x, y);
+                        }
+                    }
+                }
+            }
+            //Mouse moving left
+            else
+            {
+                m = (float)(startY - endY) / (float)(startX - endX);
+                b = startY - (m * startX);
+                if (startY < endY)
+                {
+                    for (int x = startX; x >= endX; x--)
+                    {
+                        botY = (int)Math.Floor((m * (x + 1)) + b);
+                        topY = (int)Math.Floor((m * x) + b);
+                        for (int y = botY; y <= topY; y++)
+                        {
+                            DrawPixel(x, y);
+                        }
+                    }
+                }
+                //Mouse moving up
+                else
+                {
+                    for (int x = startX; x >= endX; x--)
+                    {
+                        botY = (int)Math.Floor((m * x) + b);
+                        topY = (int)Math.Floor((m * (x + 1)) + b);
+                        for (int y = topY; y >= botY; y--)
+                        {
+                            DrawPixel(x, y);
+                        }
+                    }
+                }
+            }
+        }
+
+        /*--PanelImage stuff--*/
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDraw = true;
+            DrawPixel(e.X, e.Y);
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDraw = false;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDraw)
+            {
+                DrawLineBetweenPoints(prevMouseX, prevMouseY ,e.X, e.Y);
+            }
+        }
+
+        private void pickColorBtn_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            MyDialog.Color = drawColor;
+
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+            {
+                drawColor = MyDialog.Color;
+                colorDisplayTextBox.BackColor = drawColor;
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:/Users/Student/Desktop";
+            openFileDialog.Filter = "myimage files (*.myimage)|*.myimage";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                string filePath = openFileDialog.FileName;
+
+                //Read the contents of the file into a stream
+                Stream fileStream = openFileDialog.OpenFile();
+                
+                //StreamReader reader = new StreamReader(fileStream);
+                //string fileContent = reader.ReadToEnd();
+                //string[] fileContents = fileContent.Split(' ');
+                //if (fileContents.Length != 3)
+                //{
+                //    Console.WriteLine("Open file failed. Could not read properly");
+                //    fileStream.Close();
+                //    return;
+                //}
+
+                //int imageWidth = int.Parse(fileContents[0]);
+                //int imageHeight = int.Parse(fileContents[1]);
+                //panelImageBitmap = new Bitmap(imageWidth, imageHeight);
+
+                fileStream.Position = 0;
+                List<byte> imageWidthBytes = new List<byte>();
+                List<byte> imageHeightBytes = new List<byte>();
+                List<byte> imageBytes = new List<byte>();
+                int currentList = 0;
+                while (fileStream.Position < fileStream.Length)
+                {
+                    int intByte = fileStream.ReadByte();
+                    if (intByte == -1)
+                        break;
+                    byte b = (byte)intByte;
+                    //32 is space
+                    if (b == 32)
+                        currentList++;
+                    else
+                    {
+                        switch (currentList)
+                        {
+                            case 0:
+                                imageWidthBytes.Add(b);
+                                break;
+                            case 1:
+                                imageHeightBytes.Add(b);
+                                break;
+                            case 2:
+                                imageBytes.Add(b);
+                                break;
+                        }
+                    }
+                }
+
+                if (currentList > 2)
+                {
+                    Console.WriteLine("Open file failed. Could not read properly");
+                    fileStream.Close();
+                    return;
+                }
+
+                int imageWidth = int.Parse(Encoding.UTF8.GetString(imageWidthBytes.ToArray()));
+                int imageHeight = int.Parse(Encoding.UTF8.GetString(imageHeightBytes.ToArray()));
+                panelImageBitmap = new Bitmap(imageWidth, imageHeight);
+
+                int i = 0;
+                for (int y = 0; y < imageHeight; y++)
+                {
+                    for (int x = 0; x < imageWidth; x++)
+                    {
+
+                        byte R = imageBytes[i + 0];
+                        byte G = imageBytes[i + 1];
+                        byte B = imageBytes[i + 2];
+                        byte A = imageBytes[i + 3];
+                        panelImageBitmap.SetPixel(x, y, Color.FromArgb(A, R, G, B));
+                        i += 4;
+                    }
+                }
+                fileStream.Close();
+                UpdatePanelImage();
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "myimage files (*.myimage)|*.myimage";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog.OpenFile()) != null)
+                {
+                    StreamReader reader = new StreamReader(myStream);
+                    // Code to write the stream goes here.
+                    string imageWidth = panelImageBitmap.Width.ToString();
+                    string imageHeight = panelImageBitmap.Height.ToString();
+                    
+                    byte[] imageBytes = new byte[panelImageBitmap.Width * panelImageBitmap.Height * 4];
+                    int i = 0;
+                    for (int y = 0; y < panelImageBitmap.Height; y++)
+                    {
+                        for (int x = 0; x < panelImageBitmap.Width; x++)
+                        {
+                            imageBytes[i + 0] = panelImageBitmap.GetPixel(x, y).R;
+                            imageBytes[i + 1] = panelImageBitmap.GetPixel(x, y).G;
+                            imageBytes[i + 2] = panelImageBitmap.GetPixel(x, y).B;
+                            imageBytes[i + 3] = panelImageBitmap.GetPixel(x, y).A;
+                            i += 4;
+                        }
+                    }
+
+                    myStream.Write(Encoding.UTF8.GetBytes(imageWidth), 0, imageWidth.Length);
+                    myStream.Write(Encoding.UTF8.GetBytes(" "), 0, 1);
+                    myStream.Write(Encoding.UTF8.GetBytes(imageHeight), 0, imageWidth.Length);
+                    myStream.Write(Encoding.UTF8.GetBytes(" "), 0, 1);
+                    myStream.Write(imageBytes, 0, imageBytes.Length);
+                    myStream.Close();
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e);
+        }
+    }
+}
